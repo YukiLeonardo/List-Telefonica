4feedc93e605fc95208d55f6510ae092cc97029e
Leonardo <802.293@alunos.unigran.br>
2022-09-15 20:52:34 -0300

    Salvar e Contato

--- app/src/main/java/br/unigran/listatelefonica/Contatos.java
+++ app/src/main/java/br/unigran/listatelefonica/Contatos.java
@@ -1,2 +1,52 @@
-package br.unigran.listatelefonica;public class Contatos {
+package br.unigran.listatelefonica;
+
+public class Contatos {
+    private Integer id;
+    private String nome;
+    private String telefone;
+    private String datanasc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDatanasc() {
+        return datanasc;
+    }
+
+    public void setDatanasc(String datanasc) {
+        this.datanasc = datanasc;
+    }
+
+    @Override
+    public String toString() {
+        return "Contatos{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", datanasc='" + datanasc + '\'' +
+                '}';
+    }
 }
+
+
--- app/src/main/java/br/unigran/listatelefonica/MainActivity.java
+++ app/src/main/java/br/unigran/listatelefonica/MainActivity.java
@@ -3,12 +3,44 @@ package br.unigran.listatelefonica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nome;
+    EditText telefone;
+    EditText datanasc;
+    ListView listagem;
+    List<Contatos> dados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.id_nome);
+        telefone = findViewById(R.id.id_telefone);
+        datanasc = findViewById(R.id.id_datanasc);
+        listagem = findViewById(R.id.id_list);
+        dados = new ArrayList();
+        ArrayAdapter adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dados);
+        listagem.findViewById(id.R)
     }
+    public void salvar (View view){
+        Contatos contatos = new Contatos();
+        contatos.setNome (nome.getText().toString());
+        contatos.setTelefone(telefone.getText().toString());
+        contatos.setDatanasc(datanasc.getText().toString());
+    }
+
+
+
+}
+
 }
